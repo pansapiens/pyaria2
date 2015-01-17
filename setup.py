8ff3c94bcc1c8bfced10556613cfe611a542efdd
alfateam123 <alfateam123@gmail.com>
2015-01-17 19:42:17 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='PyAria2',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.2',
+
+    description='library for accessing aria2c via XML-RPC interface',
+    long_description='library for accessing aria2c via XML-RPC interface',
+
+    # The project's main homepage.
+    url='https://github.com/alfateam123/pyaria2',
+
+    # Author details
+    author='alfateam123',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Development',
+        'Topic :: Communications :: File Sharing',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+
+    #test_suite="tests"
+)
